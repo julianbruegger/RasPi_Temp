ff304eba86b35769b1480b0e5e44112c0b09f976
Julian Brügger <brj@ICT-BZ.ch>
2020-01-27 09:33:39 +0100

    Corrected Installscript

--- 01_Setup/setup.sh
+++ 01_Setup/setup.sh
@@ -5,8 +5,10 @@ sleep 1
 echo "Updates Heruntergeladen und Installiert!"
 echo ""
 echo ""
-echo "Webserver wird Installiert!"
+echo "DHT11 Zusatzpakete Installieren"
 
-sudo apt-get install apache2
+sudo apt-get install build-essential python-dev python-openssl git
+git clone https://github.com/adafruit/Adafruit_Python_DHT.git && cd Adafruit_Python_DHT
+sudo python setup.py install
 
 echo "Alles wichtige ist nun Installiert"
